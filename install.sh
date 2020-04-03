91e426d0780ce2568a9eac7d0c24772b0be85b00
golark <firatcnur@gmail.com>
2020-04-02 21:44:12 -0300

    upgraded install.sh with service registration functionality

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 # utask installation script
 #
@@ -12,8 +12,13 @@ set -e
 # Make sure the contentss of the script is the same as
 # https://github.com/golark/utaskdaemon/install/install_utask.sh
 # before executing
-INSTALL_DIR="/opt/utask"
 
+# top level definitions
+TOP_DIR=$(dirname "$0")
+INSTALL_DIR="/opt/utask"
+SERVICE_NAME="utaskdaemon.service"
+SERVICE_FILE="$TOP_DIR/$SERVICE_NAME"
+USER_SERVICE_DIR=~/.config/systemd/user
 
 # distribution check
 get_distribution() {
@@ -27,8 +32,75 @@ get_distribution() {
 	echo "$dist"
 }
 
+# pause for a few seconds determined by argument 1 while printing dots
+sleep_with_dots() {
+
+  for ((i=1; i<=$1; i++))
+    do
+      printf "."
+      sleep 1
+    done
+  printf "\n"
+
+}
+
+# stop any active services with the given trace
+# this must be done before installingthe binary as well - running service can lock the binary
+# also do this before installing a new service with the same footprint
+stop_active_service() {
+
+  serviceName=$1
+
+  # stop any potential backround service ( both user and system )
+  # check user services
+  if (systemctl is-active --quiet --user $serviceName); then
+    systemctl stop --user $serviceName
+    echo "stopped user service $serviceName"
+  fi
+
+  # now check system services
+  if (systemctl is-active --quiet $serviceName); then
+    systemctl stop $serviceName
+    echo "stopped system service $serviceName"
+  fi
+
+}
+
+# register the service with service manager ( systemd in this case )
+register_service() {
+
+  # step 1 - stop any potential backround utaskdaemon service
+  stop_active_service $SERVICE_NAME
+
+  echo "######################################################"
+  # step 2 - install service
+  echo "registering service"
+  mkdir -p $USER_SERVICE_DIR
+
+  echo $SERVICE_FILE
+  pwd
+
+  cp $SERVICE_FILE $USER_SERVICE_DIR
+
+  # step 3 - reload user services and start service
+  printf "reloading user services"
+  systemctl daemon-reload --user
+  sleep_with_dots 3
+  printf "starting service"
+  systemctl start --user $SERVICE_NAME
+
+  # step 3 - short pause & check service states
+  sleep_with_dots 3
+  if systemctl is-active --quiet --user $SERVICE_NAME; then
+    echo "$SERVICE_NAME status: active"
+  fi
+
+  echo "######################################################"
+
+}
+
 # check prerequisites and install
-perform_install() {
+install_app() {
 
   echo "checking prerequisites"
 
@@ -58,19 +130,23 @@ perform_install() {
   echo "######################################################"
   echo "generating binary utaskdaemon"
   # step 4 - generate binary
-  rm -rf ./bin && mkdir ./bin && cd ./bin
-  ret=$(go build ..)
+  rm -rf ./bin && mkdir ./bin
+  ret=$(go build -o ./bin)
 
   # step 5 - install binary
   sudo mkdir -p $INSTALL_DIR                  # create installation folder
   sudo chown $USER:$USER $INSTALL_DIR      # set permission/group to current user
-  echo "installing new binary $INSTALL_DIR"
-  cp utaskdaemon $INSTALL_DIR    # copy binary
-  cp ../config.yaml $INSTALL_DIR # copy config file
+  echo "installing utaskdaemon binary"
+  cp ./bin/utaskdaemon $INSTALL_DIR    # copy binary
+  cp ./config.yaml $INSTALL_DIR # copy config file
 
-  exit 0
+  echo "application installed to $INSTALL_DIR"
 }
 
-# bon voyage comrades
-perform_install
+# stop service is active
+stop_active_service $SERVICE_NAME
+# first install the application - bon voyage comrades!
+install_app
+# now install as service
+register_service
 
